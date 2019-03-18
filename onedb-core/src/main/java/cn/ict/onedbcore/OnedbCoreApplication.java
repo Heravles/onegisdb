865584b03dee12bd02fcba3d777f6c05892411af
root <root@DESKTOP-I8LCMIA.localdomain>
2019-03-18 19:33:55 +0800

    文件传功能，测试

--- onedb-core/src/main/java/cn/ict/onedbcore/OnedbCoreApplication.java
+++ onedb-core/src/main/java/cn/ict/onedbcore/OnedbCoreApplication.java
@@ -3,9 +3,15 @@ package cn.ict.onedbcore;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import cn.ict.onedbcore.filetransfer.FileStorageProperties;
 
 @MapperScan("cn.ict.onedbcore.mapper")
 @SpringBootApplication
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class OnedbCoreApplication {
 
 	public static void main(String[] args) {
---
+++ onedb-core/src/main/java/cn/ict/onedbcore/service/FileStorageService.java
@@ -0,0 +1,80 @@
+package cn.ict.onedbcore.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import cn.ict.onedbcore.exception.FileStorageException;
+import cn.ict.onedbcore.exception.MyFileNotFoundException;
+import cn.ict.onedbcore.filetransfer.FileStorageProperties;
+
+@Service
+public class FileStorageService {
+	
+	private final Path fileStorageLocation;
+	
+	 @Autowired
+	    public FileStorageService(FileStorageProperties fileStorageProperties) throws Exception {
+	        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
+	                .toAbsolutePath().normalize();
+
+	        try {
+	            Files.createDirectories(this.fileStorageLocation);
+	        } catch (Exception ex) {
+	            // throw new Exception("service construction exception");
+	        	System.out.println("service construction exception");
+	        	// throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", ex); 
+	        }
+	    }
+	 
+	 public String storeFile(MultipartFile file)  {
+	        // Normalize file name
+	        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+	        try {
+	            // Check if the file's name contains invalid characters
+	            if(fileName.contains("..")) {
+	                System.out.println("Sorry! Filename contains invalid path sequence " + fileName);
+	            }
+
+	            // Copy file to the target location (Replacing existing file with the same name)
+	            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+	            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+	            return fileName;
+	        } catch (IOException ex) {
+	            //throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+	        	 throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+	        }
+	    }
+	 
+	 
+	 public Resource loadFileAsResource(String fileName) {
+	        try {
+	            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+	            Resource resource = new UrlResource(filePath.toUri());
+	            if(resource.exists()) {
+	                return resource;
+	            } else {
+	                throw new MyFileNotFoundException("File not found " + fileName);
+	            }
+	        } catch (MalformedURLException ex) {
+	            throw new MyFileNotFoundException("File not found " + fileName, ex);
+	        }
+	    }
+
+
+
+}
